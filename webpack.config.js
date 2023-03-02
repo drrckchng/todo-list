6f9dc1beddbbb794bfe2c93b030c65f95ff03706
drrckchng <19944575+drrckchng@users.noreply.github.com>
2023-03-02 18:46:58 +0900

    Change directory for template HTML webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Todo List',
-      template: './src/assets/index.html',
+      template: './src/templates/index.html',
     }),
   ], 
   devtool: 'inline-source-map',
