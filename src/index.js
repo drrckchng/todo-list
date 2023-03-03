295ec69f167c3cf2fdff485c00fe5605290dbab4
drrckchng <19944575+drrckchng@users.noreply.github.com>
2023-03-03 12:43:23 +0900

    Add Class constructor for Tasks

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import './style.css';
 import "@fontsource/lato";
 import "@fontsource/lato/900.css";
+//import {Task} from './taskClass.js';
+
---
+++ src/taskClass.js
@@ -0,0 +1,12 @@
+export class Task {
+  constructor(name, desc, date, starred) {
+    this.name = name;
+    this.desc = desc;
+    this.date = date;
+    this.starred = starred;
+  }
+  // Method to check date
+  // Method to change name
+  // Method to change desc
+}
+
