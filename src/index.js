da314a26dd624640d4469d9436f34bd468b55535
drrckchng <19944575+drrckchng@users.noreply.github.com>
2023-03-06 18:24:56 +0900

    Comment out import

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import "@fontsource/lato";
 import "@fontsource/lato/900.css";
 import {Task} from './taskClass.js';
 import {Project} from './projectClass.js';
-
+// import * from './manipulateDOM.js';
 
 const initialProject = new Project("Todo List");
 const testTask = new Task("Test task", "Please do ASAP", "2023-03-03", true);
