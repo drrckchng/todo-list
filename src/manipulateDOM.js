74f03c5b06d5634e6e294140db0562bbe90cdd49
drrckchng <19944575+drrckchng@users.noreply.github.com>
2023-03-06 18:24:50 +0900

    Change file name

---
+++ src/manipulateDOM.js
@@ -0,0 +1,10 @@
+// Add import for task filter script
+
+function addListeners() {
+  // Grab elements and add event listeners
+  const filterAllTasks = document.getElementById("filter-all");
+  const filterTodayTasks = document.getElementById("filter-today");
+  const filterWeekTasks = document.getElementById("filter-week");
+  const filterStarred = document.getElementById("filter-starred");
+}
+
