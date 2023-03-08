bc467a3e7ea84da01001e0863218d476745733e2
drrckchng <19944575+drrckchng@users.noreply.github.com>
2023-03-09 08:37:38 +0900

    Test modular change

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 import './style.css';
 import "@fontsource/lato";
 import "@fontsource/lato/900.css";
-import {Task} from './taskClass.js';
-import {Project} from './projectClass.js';
+// import {Task} from './taskClass.js';
+// import {Project} from './projectClass.js';
+import {projectsTracker, createInitialProject} from './createItems.js';
 // import * from './manipulateDOM.js';
+//
 
-// Create initial project on load
-const initialProject = new Project("Todo List");
-// Create test task for testing purposes
-const testTask = new Task("Test task", "Please do ASAP", "2023-03-03", true);
-// Push test task into initial project
-initialProject.addTask(testTask);
+createInitialProject();
+console.log(projectsTracker);
 
-// Print project
-console.log("test");
-console.log(initialProject.tasks);
+// Call upon function to set event listeners from manipulateDOM
+  // From there, have it call a function to return filtered list of tasks
 
+ 
