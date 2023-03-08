cf82eb2027230ee4be0b44500441237e630e47d1
drrckchng <19944575+drrckchng@users.noreply.github.com>
2023-03-09 07:29:07 +0900

    Add comments for context

--- src/index.js
+++ src/index.js
@@ -5,8 +5,14 @@ import {Task} from './taskClass.js';
 import {Project} from './projectClass.js';
 // import * from './manipulateDOM.js';
 
+// Create initial project on load
 const initialProject = new Project("Todo List");
+// Create test task for testing purposes
 const testTask = new Task("Test task", "Please do ASAP", "2023-03-03", true);
+// Push test task into initial project
 initialProject.tasks.push(testTask);
+
+// Print project
 console.log(initialProject);
+console.log(initialProject.tasks);
 
