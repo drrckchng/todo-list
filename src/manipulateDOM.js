5be8a815c4498c9c8780c6a4448912bf01016067
drrckchng <19944575+drrckchng@users.noreply.github.com>
2023-03-15 16:14:54 +0900

    Add click listener that calls filter function from imported module

--- src/manipulateDOM.js
+++ src/manipulateDOM.js
@@ -1,16 +1,19 @@
 // Add import for task filter script
+import {filterTasks} from './filter.js';
 
-function addListeners() {
+export function addListeners() {
   // Grab elements and add event listeners
-  const filterAllTasks = document.getElementById("filter-all");
-  const filterTodayTasks = document.getElementById("filter-today");
-  const filterWeekTasks = document.getElementById("filter-week");
-  const filterStarred = document.getElementById("filter-starred");
+  const filterTaskButtons = document.querySelector(".side-bar .tasks").children;
+
+  document.getElementById("filter-all").addEventListener("click", filterTasks);
+  // const filterTodayTasks = document.getElementById("filter-today");
+  // const filterWeekTasks = document.getElementById("filter-week");
+  // const filterStarred = document.getElementById("filter-starred");
 }
 
-function addProject() {
-  // create new div and append to sidebar
-  // create new project object (from createItems.js)
-  // get project ID
-  // add listener that will filter tasks that match project id
-}
+// function addProject() {
+//   // create new div and append to sidebar
+//   // create new project object (from createItems.js)
+//   // get project ID
+//   // add listener that will filter tasks that match project id
+// }
