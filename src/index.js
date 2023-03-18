325100ef3efa61de5623c8211a12fe51faa6360b
drrckchng <19944575+drrckchng@users.noreply.github.com>
2023-03-18 21:27:02 +0900

    Add test cases

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import "@fontsource/lato";
 import "@fontsource/lato/900.css";
 // import {Task} from './taskClass.js';
 // import {Project} from './projectClass.js';
-import { projectsTracker, createInitialProject } from './createItems.js';
+import { projectsTracker, tasksTracker, createInitialProject, createTask } from './createItems.js';
 import { addListeners } from './manipulateDOM.js';
 
 // Initial page load
 addListeners();
 createInitialProject();
+createTask();
 console.log(projectsTracker); // test purposes
+console.log(tasksTracker); // test purposes
 // Call method to select "All Tasks" view
 // Call upon function to set event listeners from manipulateDOM
   // From there, have it call a function to return filtered list of tasks
