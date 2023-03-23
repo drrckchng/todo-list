43944f53907f45216d4c39f17d463b0d4b2e55e7
drrckchng <19944575+drrckchng@users.noreply.github.com>
2023-03-23 17:37:41 +0900

    Add conditional to add cases for filter settings

--- src/filter.js
+++ src/filter.js
@@ -1,6 +1,14 @@
 import { tasksTracker } from './createItems.js';
 
 export function filterTasks(event) {
-  console.log(event.target.id);
-  console.log("filtering tasks");
+  const filterSetting = event.target.id;
+  if(filterSetting === 'filter-all') {
+    console.log(tasksTracker);
+  } else if(filterSetting === 'filter-today') {
+    console.log("filter today");
+  } else if(filterSetting === 'filter-week') {
+    console.log("filter week");
+  } else if(filterSetting === 'filter-starred') {
+    console.log("filter starred");
+  }
 }
