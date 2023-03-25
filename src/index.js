1d896d7bf2c3bd399a0fe492c814019e586b001c
drrckchng <19944575+drrckchng@users.noreply.github.com>
2023-03-26 00:38:41 +0900

    Add more test cases

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import { addListeners } from './manipulateDOM.js';
 // Initial page load
 addListeners();
 createProject("Todo List");
+createProject("Breakfast");
 
 // Click on "All Tasks" button
 window.onload = function() {
@@ -18,8 +19,11 @@ window.onload = function() {
 
 // Test cases
 createTask("Task One", "This is a task", new Date(), true, 0);
-createTask("Task One", "This is a task", new Date(2023, 2, 1), true, 0);
-createTask("Task One", "This is a task", new Date(2023, 2, 20), true, 0);
-createTask("Task One", "This is a task", new Date(2023, 2, 20), false, 0);
+createTask("Task Two", "This is a task", new Date(2023, 2, 1), true, 0);
+createTask("Task Three", "This is a task", new Date(2023, 2, 20), true, 0);
+createTask("Task Four", "This is a task", new Date(2023, 2, 20), false, 0);
+createTask("Task Four", "This is a task", new Date(2023, 2, 20), false, 0);
+createTask("Eat cereal", "This is a task", new Date(2023, 2, 20), false, 1);
+createTask("Peel apple", "This is a task", new Date(2023, 2, 20), false, 1);
 console.log(projectsTracker);
 console.log(tasksTracker);
