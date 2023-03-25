4798aa4958f1e00f4090f4edbb1fecc885d4996f
drrckchng <19944575+drrckchng@users.noreply.github.com>
2023-03-26 00:44:34 +0900

    Refactor to only call displayTask function at end of conditional check

--- src/filter.js
+++ src/filter.js
@@ -4,35 +4,34 @@ import { displayTask } from './manipulateDOM.js';
 
 // Filter tasks matching date criteria
 export function filterTasks(event) {
-
   const today = new Date();
   const filterSetting = event.target.id;
   const filteredTasks = [];
 
-  if(filterSetting === "filter-today") {
-    tasksTracker.forEach(task => {
-      if(differenceInDays(today, task.date) === 0) {
-        filteredTasks.push(task);
-      }
-    });
-    displayTask(filteredTasks);
-  } else if(filterSetting === "filter-week") {
-    tasksTracker.forEach(task => {
-      if(differenceInDays(today, task.date) <= 7) {
-        filteredTasks.push(task);
-      }
-    });
-    displayTask(filteredTasks);
-  } else if(filterSetting === "filter-starred") {
-    tasksTracker.forEach(task => {
-      if(task.starred === true) {
-        filteredTasks.push(task);
-      }
-    });
-    displayTask(filteredTasks);
-  } else if(filterSetting === "filter-all") {
+  if(filterSetting === "filter-all") {
     displayTask(tasksTracker);
-  } 
+  } else {
+    if(filterSetting === "filter-today") {
+      tasksTracker.forEach(task => {
+        if(differenceInDays(today, task.date) === 0) {
+          filteredTasks.push(task);
+        }
+      });
+    } else if(filterSetting === "filter-week") {
+      tasksTracker.forEach(task => {
+        if(differenceInDays(today, task.date) <= 7) {
+          filteredTasks.push(task);
+        }
+      });
+    } else if(filterSetting === "filter-starred") {
+      tasksTracker.forEach(task => {
+        if(task.starred === true) {
+          filteredTasks.push(task);
+        }
+      });
+    }
+    displayTask(filteredTasks);
+  }
 }
 
 // Filter tasks matching project id and call displayTask
