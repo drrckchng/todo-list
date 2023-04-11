bbf1c6e3bc758e5458a224feeb479525086a9c91
drrckchng <19944575+drrckchng@users.noreply.github.com>
2023-04-11 20:58:30 +0900

    Add task id

--- src/taskClass.js
+++ src/taskClass.js
@@ -1,9 +1,15 @@
+let taskId = 0;
 export class Task {
   constructor(name, desc, date, starred) {
     this.name = name;
     this.desc = desc;
     this.date = date;
     this.starred = starred;
+    this.taskId = taskId;
+    taskId += 1;
+  }
+  toggleStarState = function() {
+    this.starred = !starred;
   }
 }
 
